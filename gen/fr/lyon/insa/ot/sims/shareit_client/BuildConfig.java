a1b1de1009d61795fd79828820716a61d89bd80c
Louise Crepet <louise.crepet@insa-lyon.fr>
2015-02-17 14:40:47 +0100

    first commit

---
+++ gen/fr/lyon/insa/ot/sims/shareit_client/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package fr.lyon.insa.ot.sims.shareit_client;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
