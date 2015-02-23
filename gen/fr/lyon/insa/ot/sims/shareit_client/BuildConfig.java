ffa1f91ea948fa1347791555d1855a5ea760cc6f
gperrin <gaetan.perrin@insa-lyon.fr>
2015-02-23 08:53:26 +0100

    Updated gitignore

--- gen/fr/lyon/insa/ot/sims/shareit_client/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package fr.lyon.insa.ot.sims.shareit_client;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
