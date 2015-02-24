497646f29ab32dd22293a49cb4af8dc8bda49644
Louise Crepet <louise.crepet@insa-lyon.fr>
2015-02-24 10:44:42 +0100

    Add object interface

--- app/src/main/java/fr/lyon/insa/ot/sims/shareit_client/AddObjectActivity.java
+++ app/src/main/java/fr/lyon/insa/ot/sims/shareit_client/AddObjectActivity.java
@@ -23,18 +23,4 @@ public class AddObjectActivity extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
