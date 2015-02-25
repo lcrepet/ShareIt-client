bd4a8771105c77427de71c3fa4bfafe76738191e
unknown <anne.bitz@insa-lyon.fr>
2015-02-25 10:06:16 +0100

    test commit

--- app/src/main/java/fr/lyon/insa/ot/sims/shareit_client/ObjectActivity.java
+++ app/src/main/java/fr/lyon/insa/ot/sims/shareit_client/ObjectActivity.java
@@ -11,15 +11,40 @@ import android.os.AsyncTask;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import fr.lyon.insa.ot.sims.shareit_client.searchList.SearchListAdapter;
 
 
 public class ObjectActivity extends Activity {
+
+    private long id;
+    private  String TAG_ID = "id";
+    private  String TAG_NAME = "name";
+    private  String TAG_TYPE = "type";
+    private  String TAG_STATUS = "status";
+    private  String TAG_DESCRIPTION="description";
+    private  String TAG_PROP="prop";
+    JSONObject object = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_object);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            return;
+        }
+        id = extras.getLong(Intent.EXTRA_TEXT);
+        TAG_ID= String.valueOf(id);
+
+
+
 
         final Button objectButton = (Button) findViewById(R.id.bouton1);
         objectButton.setOnClickListener(new View.OnClickListener() {
@@ -51,5 +76,47 @@ public class ObjectActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class DisplayObject extends AsyncTask<String, Void, JSONObject>{
+
+        @Override
+        public JSONObject doInBackground(String... message) {
+            return Request.getRequest(Constants.uri + "product/" + TAG_ID);
+        }
+        protected void onPostExecute(JSONObject reader) {
+            TextView nom = (TextView) findViewById(R.id.NomObjet);
+            TextView type = (TextView) findViewById(R.id.TypeObjet);
+            TextView status = (TextView)findViewById(R.id.StatusObjet);
+            TextView desc = (TextView)findViewById(R.id.DescriptionObjet);
+            TextView prop = (TextView) findViewById(R.id.Proprietaire);
+
+            try {
+
+
+                JSONObject c = object.getJSONObject(TAG_ID);
+
+                String name = c.getString(TAG_NAME);
+                nom.setText((CharSequence) name);
+
+                String typ = c.getString(TAG_TYPE);
+                type.setText((CharSequence) typ);
+
+                String stat = c.getString(TAG_STATUS);
+                type.setText((CharSequence) stat);
+
+                String des = c.getString(TAG_DESCRIPTION);
+                type.setText((CharSequence) des);
+
+                String pro = c.getString(TAG_PROP);
+                type.setText((CharSequence) pro);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 
 }
