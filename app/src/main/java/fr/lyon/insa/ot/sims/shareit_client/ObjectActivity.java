1bffd26d5d5e715c3a390dbee86e8c2bcc25badc
Louise Crepet <louise.crepet@insa-lyon.fr>
2015-03-17 14:31:12 +0100

    Merge

--- app/src/main/java/fr/lyon/insa/ot/sims/shareit_client/ObjectActivity.java
+++ app/src/main/java/fr/lyon/insa/ot/sims/shareit_client/ObjectActivity.java
@@ -1,9 +1,12 @@
 package fr.lyon.insa.ot.sims.shareit_client;
 
 import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -52,7 +55,9 @@ public class ObjectActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_object);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar bar = getActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0099CC")));
+        bar.setDisplayHomeAsUpEnabled(true);
 
         Bundle extras = getIntent().getExtras();
         if (extras == null) {
